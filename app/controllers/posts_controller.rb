078b151f89bf4ead0141a44217c519a5967a3a10
Xin <Detective_boys@hotmil.com>
2018-12-30 00:46:11 +0800

    implement account/posts#destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :create, :destroy]
+  before_action :find_post_and_check_permission, only: [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -7,13 +8,6 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
-
-    if current_user != @post.user
-      redirect_to root_path, alert: "You Have No Permission"
-    end
-
   end
 
   def create
@@ -30,13 +24,6 @@ class PostsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
-
-    if current_user != @post.user
-      redirect_to root_path, alert: "You Have No Permission"
-    end
-
     if @post.update(post_params)
       redirect_to account_posts_path, notice: "Post Updated"
     else
@@ -44,7 +31,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    if @post.destroy
+      redirect_to account_posts_path, alert: "Post Deleted"
+    end
+  end
+
   private
+
+  def find_post_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You Have No Permission"
+    end
+  end
+
   def post_params
     params.require(:post).permit(:content)
   end
